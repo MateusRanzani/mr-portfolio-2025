864f5cc7b0f9e86393c66301d725c9752625f8b6
Mateus Ranzani <mateusranzani@hotmail.com>
2025-07-12 17:57:22 -0300

    update menu header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import MenuHeader from "@/components/menuHeader";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
+        <MenuHeader/>
         {children}
       </body>
     </html>
---
+++ components/menuHeader.tsx
@@ -0,0 +1,6 @@
+export default function MenuHeader() {
+    return (
+        <header className="fixed top-4 left-1/2 transform -translate-x-1/2 w-[95vw] bg-red-800">
+        </header>
+    )
+}
